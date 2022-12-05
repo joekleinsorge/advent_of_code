db8b75dd5f73b1a9513cf341d346ac50b0b38436
joeykleinsorge <josephkleinsorge@gmail.com>
2022-12-05 09:02:22 -0500

    ci:commit

---
+++ 2022/python/1-2.py
@@ -0,0 +1,34 @@
+'''
+Loop through list
+    sum ints until new line
+    if sum > highest, set sum as highest
+    elif sum > second highest, set sum as second highest
+    elif sum > third highest, set sum as third highest
+Output total of highest, second highest, third highest
+'''
+
+def main():
+    with open('1.txt') as f:
+        highest = 0
+        second_highest = 0
+        third_highest = 0
+        sum = 0
+        for line in f:
+            if line == '\n':
+                if sum > highest:
+                    third_highest = second_highest
+                    second_highest = highest
+                    highest = sum
+                elif sum > second_highest:
+                    third_highest = second_highest
+                    second_highest = sum
+                elif sum > third_highest:
+                    third_highest = sum
+                sum = 0
+                continue
+            sum += int(line)
+        return print(highest + second_highest + third_highest)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2022/python/2.py
@@ -0,0 +1,44 @@
+'''
+Loop through list
+    calculate score for each line
+    add score to total
+Output total
+'''
+
+wining_moves = {
+    'A': 'Y', # Paper beats rock
+    'B': 'Z', # Scissors beats paper
+    'C': 'X'  # Rock beats scissors
+}
+
+shape_score = {
+    'X': 1, # Rock is worth 1 point
+    'Y': 2, # Paper is worth 2 points
+    'Z': 3  # Scissors is worth 3 points
+}
+
+
+def calculate_score(line):
+    left_char = line[0].upper()
+    right_char = line[2].upper()
+
+    if left_char == right_char:
+        print(f'draw: {left_char} vs {right_char}')
+        return 3 + shape_score[right_char] # 3 points for a draw + shape score
+    if wining_moves[left_char] == right_char:
+        print(f'you win: {left_char} vs {right_char}')
+        return 6 + shape_score[right_char] # 6 points for a win + shape score
+    print(f'you lose: {left_char} vs {right_char}')
+    return 0 + shape_score[right_char] # 0 points for a loss + shape score
+
+
+def main():
+    with open('2.txt') as f:
+        total = 0
+        for line in f:
+            total += calculate_score(line)
+        return print(total)
+
+
+if __name__ == "__main__":
+    main()
